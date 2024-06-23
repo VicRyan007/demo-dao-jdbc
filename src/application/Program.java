99e577bf4241ac18d43420aeb6f62a5d97e76a59
VicRyan007 <ryannautico306@gmail.com>
2024-06-22 23:47:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import modal.entities.Department;
+import modal.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 		
 		Department obj = new Department(1,"books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
+		
+		System.out.println(seller);
 		
 	}
 }
