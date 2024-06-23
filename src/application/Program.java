1c2e1c565d85b6b5f9e5036d6db8badb39ec08e7
VicRyan007 <ryannautico306@gmail.com>
2024-06-23 00:54:17 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,15 +11,10 @@ public class Program {
 
 	public static void main(String[]args) {
 		
-		Department obj = new Department(1,"books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
-		
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
-		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 		
 	}
-}
+} 
--- src/modal/dao/DaoFactory.java
+++ src/modal/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package modal.dao;
 
+import db.DB;
 import modal.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDAO createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
 }
