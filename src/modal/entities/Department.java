8c7d6902749f416378c5a21ba90ddcbd23dc9766
VicRyan007 <ryannautico306@gmail.com>
2024-06-22 23:43:24 -0300

    Department entity class

---
+++ src/modal/entities/Department.java
@@ -0,0 +1,51 @@
+package modal.entities;
+
+import java.io.Serializable;
+
+
+public class Department implements Serializable {
+
+	private Integer id;
+	private String name;
+	
+	public Department() {
+		
+	}
+
+	public Department(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Department [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	
+	
+	
+}
